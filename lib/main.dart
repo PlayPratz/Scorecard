611f555064e2fe5002066a66ca782e8faa921bc7
PlayPratz <playpratz@gmail.com>
2024-09-29 12:07:35 +0530

    [WIP] Documentation, SQL Handler

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,49 @@ import 'package:get_it/get_it.dart';
 import 'package:scorecard/modules/repository/service/repostiory_service.dart';
 import 'package:scorecard/screens/home_screen.dart';
 
+/// Welcome to [Scorecard]! You must be new here. The architecture and structure
+/// of this application might be a little overwhelming. To help you out,
+/// here's the naming scheme followed by classes in this application:
+///
+/// [Models] are analogous to POJOs and represent a container to hold data. They
+/// strictly DO NOT comprise of any business logic. At best, they can contain
+/// helper functions that perform basic operations like concatenation of data,
+/// or adding multiple numbers in a list.
+///
+/// [Handlers] are classes that serve as wrappers for THIRD-PARTY LIBRARIES.
+/// This is to ensure that in case we switch to another library in the future,
+/// changes will be required in only one class.
+///
+/// [Repositories] are only for STORING and RETRIEVING DATA. They are
+/// essentially an abstraction of a database.
+///
+/// [Services] are STATELESS classes that perform major Business Logic
+/// operations. They are initialized once and used throughout the app. The
+/// service is responsible for all procedural work in the app, like fulfilling
+/// the pre-requisites of any operation or validating inputs.
+///
+/// [Controllers] are STATE controllers that manipulate the state of UI. Any
+/// operation that is triggered by user-input MUST go through a controller. The
+/// operation could vary from just changing the striker to creating a new match.
+/// After each such interaction, the Controller is responsible for invoking the
+/// required business logic (via a Service) and also to push state changes back
+/// to the UI.
+///
+/// [Screens] are complete screens that are visible to the user. Needless to
+/// say, they can comprise of multiple widgets. As of now, every screen
+/// utilizes the [Scaffold] widget as its root.
+///
+/// The above list is ordered such that every [Component] may import other
+/// components of the same level or any component above it, but CANNOT import
+/// any component below it. To clarify, a Model can only see other Models, and
+/// MUST NOT import a Handler, Repository, Service, Controller and of course,
+/// Screen. Similarly, a Service can import Repositories, Handlers, Models and
+/// even other Services, but not Controllers and Screens.
+///
+/// Of course this rule is to give a general idea, and usually a Screen won't
+/// gain much by importing another screen. Services and Models are the only
+/// components which may gain from importing other classes of the same kind.
+
 void main() {
   // TODO Improve
 
---
+++ lib/modules/database/sql/handlers/sql_db_handler.dart
@@ -0,0 +1,31 @@
+import 'package:sqflite/sqflite.dart';
+
+class SQLDBHandler {
+  late final Database db;
+
+  void initialize() async {
+    db = await openDatabase("");
+  }
+
+  Future<void> insert({
+    required String table,
+    required Map<String, dynamic> values,
+  }) async {
+    await db.insert(table, values);
+  }
+
+  Future<Iterable<dynamic>> query(
+      {required String table,
+      List<String> columns = const [],
+      required String where}) async {
+    final result = await db.query(table, columns: columns, where: where);
+  }
+
+  Future<void> update({
+    required String table,
+    required Map<String, Object?> values,
+    required String where,
+  }) async {
+    await db.update(table, values);
+  }
+}
